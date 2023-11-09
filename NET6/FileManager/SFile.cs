81668796d7af15764ada78969ae5652900efe7b8
sim5522 <sim5522@naver.com>
2023-11-09 16:38:11 +0900

    FileManager 폴더 기능 추가

--- NET6/FileManager/SFile.cs
+++ NET6/FileManager/SFile.cs
@@ -2,14 +2,57 @@
 {
     public class SFile
     {
-        // file 검사
+        #region [File]
 
-        // folder 검사
+        #endregion
 
-        // file open dialog
+        #region [Folder] 
+        /// <summary>
+        /// 폴더 존재여부 검사
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public bool ExistsDirectory(string path)
+        {
+            return Directory.Exists(path) ? true : false;
+        }
 
-        // dialog
+        /// <summary>
+        /// 폴더 생성
+        /// </summary>
+        /// <param name="path"></param>
+        public void CreateFolder(string path)
+        {
+            if (!ExistsDirectory(path))
+                Directory.CreateDirectory(path);
+        }
 
-        public static void 
+        /// <summary>
+        /// 폴더 삭제
+        /// </summary>
+        /// <param name="path"></param>
+        public void DeleteFolder(string path)
+        {
+            if (ExistsDirectory(path))
+                Directory.Delete(path, true);
+        }
+
+        /// <summary>
+        /// 소스폴더 내부 파일을 목적지폴더에 복사
+        /// </summary>
+        /// <param name="srcFolderPath"></param>
+        /// <param name="destFolderPath"></param>
+        public void CopyFileFromFolder(string srcFolderPath, string destFolderPath)
+        {
+            CreateFolder(destFolderPath);
+
+            foreach (string file in Directory.GetFiles(srcFolderPath, "*.*", SearchOption.AllDirectories))
+            {
+                string destinationFilePath = file.Replace(srcFolderPath, destFolderPath);
+
+                File.Copy(file, destinationFilePath);
+            }
+        }
+        #endregion
     }
 }
